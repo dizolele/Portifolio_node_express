f0bd4ced09eb2e132a03a6f1e113a3fa54cd393b
Dizolele Pedro <dizolele.pedro@gmail.com>
2021-09-14 21:20:48 -0300

    Incluindo as rotas

--- route/router.js
+++ route/router.js
@@ -2,13 +2,47 @@ const express = require("express");
 const router = express.Router();
 const projetoSchema = require("../models/projeto");
 
-// router.get("/create", async (req, res) => {
-//     const projetoschema = await projetoSchema.create(
-//             {nome: "Projeto Blog", link: "https://github.com/dizolele/Projeto_Blog-backend",
-//              descricao: "Trabalhando as operaçôes CRUD" }
-//             )
+router.get("/create", async (req, res) => {
+    const projetoschema = await projetoSchema.create(
+            {nome: "Projeto Blog", link: "https://github.com/dizolele/Projeto_Blog-backend",
+             descricao: "Trabalhando as operaçôes CRUD" }
+            )
             
-//     res.json({ projetoschema })
-// })
+    res.json({ projetoschema })
+})
+
+router.get("/read", async (req, res) => {
+    const projetoschema = await projetoSchema.find({ })
+
+    res.json({ projetoschema })
+})
+
+router.get("/read/:id", async (req, res) => {
+    const projetoschema = await projetoSchema.findById(req.params.id)
+
+    res.json({ projetoschema })
+})
+
+router.get("/update/:id", async (req, res) => {
+    const projetoschema = await projetoSchema.findById(req.params.id)
+
+    projetoschema.nome = "brincando mongoose"
+    projetoschema.link = "https://github.com/dizolele/Brincando_Monggose_DB"
+    projetoschema.descricao = "conhecendo o mongoose"
+
+    await projetoschema.save()
+
+    res.json({ projetoschema })
+})
+
+router.get("/delete/:id", async (req, res) => {
+    const projetoschema = await projetoSchema.deleteOne({ _id: req.params.id })
+
+    res.send("Deletado")
+})
+
+
+
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const methodOverride = require("method-override");
 const mongoose = require("mongoose");
 const projetoSchema = require("./models/projeto");
-//const rota = require("./route/router");
+const router = require("./route/router");
 
 
 mongoose.connect('mongodb://localhost/portifolio', {useNewUrlParser:true, 
@@ -21,50 +21,50 @@ app.get("/", (req, res) => {
     res.render("index", { text: "Dizolele Pedro"})
 })
 
-app.get("/create", async (req, res) => {
-    const projetoschema = await projetoSchema.create(
-            {nome: "Projeto Blog", link: "https://github.com/dizolele/Projeto_Blog-backend",
-             descricao: "Trabalhando as operaçôes CRUD" }
-            )
+// app.get("/create", async (req, res) => {
+//     const projetoschema = await projetoSchema.create(
+//             {nome: "Projeto Blog", link: "https://github.com/dizolele/Projeto_Blog-backend",
+//              descricao: "Trabalhando as operaçôes CRUD" }
+//             )
             
-    res.json({ projetoschema })
-})
+//     res.json({ projetoschema })
+// })
 
-app.get("/read", async (req, res) => {
-    const projetoschema = await projetoSchema.find({})
+// app.get("/read", async (req, res) => {
+//     const projetoschema = await projetoSchema.find({})
 
-    res.json({ projetoschema })
-})
+//     res.json({ projetoschema })
+// })
 
-app.get("/read/:id", async (req, res) => {
-    const projetoschema = await projetoSchema.findById(req.params.id)
-    res.json({ projetoschema })
-})
+// app.get("/read/:id", async (req, res) => {
+//     const projetoschema = await projetoSchema.findById(req.params.id)
+//     res.json({ projetoschema })
+// })
 
-app.get("/update/:id", async (req, res) => {
-    const projetoschema = await projetoSchema.findById(req.params.id)
+// app.get("/update/:id", async (req, res) => {
+//     const projetoschema = await projetoSchema.findById(req.params.id)
 
-    projetoschema.nome = "Typescript"
-    projetoschema.link = "https://github.com/dizolele/Exercicio-de-POO"
-    projetoschema.descricao = "Exercicio de objetos"
+//     projetoschema.nome = "Typescript"
+//     projetoschema.link = "https://github.com/dizolele/Exercicio-de-POO"
+//     projetoschema.descricao = "Exercicio de objetos"
 
-    await projetoschema.save()
+//     await projetoschema.save()
 
-    res.json({projetoschema})
-})
+//     res.json({projetoschema})
+// })
 
-app.get("/delete/:id", async (req, res) => {
-    projetoschema = await projetoSchema.deleteOne({ _id: req.params.id }, () => {
-        console.log("deletado no banco de dados mongodb")
-    })
+// app.get("/delete/:id", async (req, res) => {
+//     projetoschema = await projetoSchema.deleteOne({ _id: req.params.id }, () => {
+//         console.log("deletado no banco de dados mongodb")
+//     })
 
-    res.send("deletado")
-})
-
-
+//     res.send("deletado")
+// })
 
 
 
+
+app.use(router);
 
 app.listen(port, () => {
     console.log(`Rodando o servidor express no localhost porta ${port}`)
